3cff059d487475d53405f700971da1ea4e34bc0f
mh11 <mhaimel@cemm.oeaw.ac.at>
2020-09-18 14:31:47 +0200

    Json add formating options

--- bsf/analyses/variant_calling.py
+++ bsf/analyses/variant_calling.py
@@ -3888,10 +3888,11 @@ class VariantCallingGATK(Analysis):
                     pickler.dump(pickler_dict_align_lane)
 
                 import jsonpickle
+                jsonpickle.set_encoder_options('json', sort_keys=True, indent=4)
 
                 with open(file=json_path, mode='w') as json_fh:
                     runnableJSON = jsonpickle.encode(pickler_dict_align_lane, unpicklable=False)
-                    json_fh.write(json.dumps(runnableJSON, indent=4, sort_keys=True))
+                    json_fh.write(runnableJSON)
 
                 # Create a bsf_run_bwa.py job to run the pickled object.
 
